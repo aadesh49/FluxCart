6821620fafe470cd01a01cebdb55e0180e979d09
Aadesh Srivastav <aadeshsrivastav49@gmail.com>
2025-08-27 18:50:17 +0530

    first commit

---
+++ backend/src/main/java/com/aadesh/FluxCart/FluxCartApplication.java
@@ -0,0 +1,14 @@
+package com.aadesh.FluxCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxCartApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxCartApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/aadesh/FluxCart/FluxCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aadesh.FluxCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
