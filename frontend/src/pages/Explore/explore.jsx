233eab2efeca8735347a801f2e2736449627cb75
Aadesh Srivastav <aadeshsrivastav49@gmail.com>
2025-09-15 23:13:24 +0530

    frontend: implement Explore page layout

--- frontend/src/pages/Explore/explore.jsx
+++ frontend/src/pages/Explore/explore.jsx
@@ -1,7 +1,30 @@
+import './explore.css';
+
 const Explore = () => {
     return (
-        <>
-        </>
+        <div className="explore-containter text-light">
+            <div className="left-column">
+                <div className="first-row" style={{overflowY: 'auto'}}>
+                    Categories
+                </div>
+                <hr className="horizontail-line" />
+                <div className="second-row" style={{overflowY: 'auto'}}>
+                    Items
+                </div>
+            </div>
+            <div className="right-column d-flex flex-column">
+                <div className="customer-form-" style={{height: '15%'}}>
+                    Customer form
+                </div>
+                <hr className="my-3 text-light" />
+                <div className="cart-items-container" style={{height: '55%', overflowY: 'auto'}}>
+                    Cart items
+                </div>
+                <div className="cart-summary container" style={{height: '30%'}}>
+                    Cart summary
+                </div>
+            </div>
+        </div>
     )
 }
 
